b931dd478a28d0279336836b94b379e610b2cf52
Paulius Dambrauskas <82829703+Paulius101@users.noreply.github.com>
2021-10-29 04:06:15 +0300

    website opening effect

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,16 +6,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AlausListComponent } from './components/alaus-list/alaus-list.component';
 import { AlusItemComponent } from './components/alus-item/alus-item.component';
+import { OpeningEffectComponent } from './components/opening-effect/opening-effect.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AlausListComponent,
     AlusItemComponent,
+    OpeningEffectComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,CarouselModule 
+    AppRoutingModule,CarouselModule, 
   ],
   providers: [],
   bootstrap: [AppComponent]
