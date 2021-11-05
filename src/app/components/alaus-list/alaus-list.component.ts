bc10100cee8e6e8b0e20cc1b2f572f248816eef3
Paulius Dambrauskas <82829703+Paulius101@users.noreply.github.com>
2021-11-05 18:03:12 +0200

    add/remove favorites moved to a service

--- src/app/components/alaus-list/alaus-list.component.ts
+++ src/app/components/alaus-list/alaus-list.component.ts
@@ -61,18 +61,4 @@ export class AlausListComponent implements OnInit {
     this.order = 'name'
   }
 
-
-  public addToFavorites(itemId: number) {
-    this.favoritesService.favorites = [...this.favoritesService.favorites, itemId];
-  }
-
-  public removeFromFavorites(itemId: number) {
-    const itemIndex = this.favoritesService.favorites.indexOf(itemId);
-
-    if (itemIndex !== -1)
-      this.favoritesService.favorites.splice(itemIndex, 1);
-
-    this.favoritesService.favorites = [...this.favoritesService.favorites];
-  }
-
 }
--- src/app/services/favorites.service.ts
+++ src/app/services/favorites.service.ts
@@ -6,4 +6,17 @@ import { Injectable } from '@angular/core';
 export class FavoritesService {
  public favorites: any[] = [];
   constructor() { }
+
+  public addToFavorites(itemId: number) {
+    this.favorites = [...this.favorites, itemId];
+  }
+
+ public removeFromFavorites(itemId: number) {
+    const itemIndex = this.favorites.indexOf(itemId);
+
+    if (itemIndex !== -1)
+      this.favorites.splice(itemIndex, 1);
+
+    this.favorites = [...this.favorites]; 
+  }
 }
